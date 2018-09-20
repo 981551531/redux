5eeef0b757715bb87ee883d9f51b2d9f711bf633
pguochen <981551531@qq.com>
2018-09-20 17:30:10 +0800

    3543

--- app/components/newHome/topMenu.js
+++ app/components/newHome/topMenu.js
@@ -1,14 +1,17 @@
 import React, {Component} from "react";
 import "../../static/bootstrap/css/bootstrap.min.css"
 import {Link} from 'react-router-dom'
+import style from "./style.less"
 
 class TopMenu extends Component {
     render() {
         return (
             <React.Fragment>
                 <div className="container">
-                    <div className="row">
-                        <div className="col-xs-6 text-left " onClick={this.goBackHander.bind(this)}>返回</div>
+                    <div className="row" ID={style.topMenu}>
+                        <div className="col-xs-6 text-left " onClick={this.goBackHander.bind(this)}><i
+                            className="glyphicon glyphicon-menu-left"></i>返回
+                        </div>
                         <div className="col-xs-6  text-right"><Link to="/"><i className="glyphicon glyphicon-home"></i>首页</Link>
                         </div>
                         {/*       <img src={require("../../static/img/banner_02.jpg")} />*/}
